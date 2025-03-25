2f9659a368c5f4c7b216fe1dfccb15bc1e02fbc6
Akshitha <akshitha@gmail.com>
2025-03-24 19:15:10 -0500

    first commit

---
+++ formToAPI.js
@@ -0,0 +1,20 @@
+function formToApi(event, typeOfSending) {
+
+    event.preventDefault()
+
+    var data = {
+        typeOfSending: typeOfSending,
+        destinationEmail: document.getElementsByName('email')[0].value,
+        phoneNumber: document.getElementsByName('sms')[0].value,
+        message: document.getElementsByName('message')[0].value
+    }
+
+    fetch( "https://fp34n0w7z6.execute-api.us-east-1.amazonaws.com/sendingstage/sending" , {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+        mode: "no-cors"
+    })
+}
